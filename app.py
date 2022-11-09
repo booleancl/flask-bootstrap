51ff9f0c9a271ec92c2ca0ca7161913cad54e70b
Sebastián <sebastian@boolean.cl>
2022-11-09 00:29:31 -0300

    routes

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
 
+print(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hola Mundo'
+    return __name__
+
+@app.route('/name/<name>')
+def name(name):
+    return f'Hola {name}!'
+
+@app.route('/req')
+def req():
+    user_agent = request.headers.get('User-Agent')
+    return f'Tu navegador es: {user_agent}'
+
+@app.route('/routes')
+def routes():
+    print(app.url_map)
+    return "Revisa la terminal para ver las rutas"
+    
