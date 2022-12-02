8743bc81837a86f178d469c5dd85919d81a02497
sebastian <sebastian@boolean.cl>
2022-12-01 21:23:05 -0300

    enviroment config

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
-from config import Config
+from flask import Flask
+from config import config
 
 from app.extensions import db, migrate, login_manager
 
-def create_app(config_class=Config):
+def create_app(config_class=config['development']):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
@@ -12,13 +12,17 @@ def create_app(config_class=Config):
     migrate.init_app(app,db)
     login_manager.init_app(app)
 
-    # Register blueprints here
+    # Register blueprints here:
+
+    ## Main blueprint 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
+    ## Messages Blueprint 
     from app.messages import bp as messages_bp
     app.register_blueprint(messages_bp, url_prefix='/messages')
 
+    ## Authentication Blueprint
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
