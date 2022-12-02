52341588229ae627b722130d5e99beca1227451f
sebastian <sebastian@boolean.cl>
2022-12-02 00:20:55 -0300

    auth half step

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,7 +1,24 @@
-from flask import render_template
+from flask import render_template, request, url_for, redirect, flash
+from flask_login import login_user, login_required, logout_user
 from app.auth import bp
 from app.models.user import User
 
-@bp.route('/login')
+@bp.route('/login', methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(email=request.form['email']).first()
+        if user is not None and user.verify_password(request.form['password']):
+            login_user(user, request.form['remember_me'])
+            next = request.args.get('next')
+            if next is None or not next.startswith('/'):
+                next = url_for('main.index')
+            return redirect(next)
+        flash('Usuario o password incorrecto.')
     return render_template('auth/login.html')
+
+@bp.route('logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Sesión cerrada')
+    return redirect('main.index')
--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from app.extensions import db
+from app.extensions import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
@@ -7,6 +7,7 @@ class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
 
     @property
@@ -22,3 +23,7 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
